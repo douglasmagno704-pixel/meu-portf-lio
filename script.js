696bcc14aa56678002f7841a381d2b58a0a6b05c
douglasmagno704-git <douglasmagno704@gmail.com>
2026-06-15 11:10:05 -0300

    atualizei a sessão sobre

--- script.js
+++ script.js
@@ -261,6 +261,34 @@ document.querySelectorAll('.projeto-card').forEach(card => {
 
 });
 
+document.querySelectorAll('.projeto-card')
+.forEach(card => {
+
+    card.addEventListener('mousemove', e => {
+
+        const rect =
+        card.getBoundingClientRect();
+
+        const x =
+        e.clientX - rect.left;
+
+        const y =
+        e.clientY - rect.top;
+
+        card.style.setProperty(
+            '--x',
+            `${x}px`
+        );
+
+        card.style.setProperty(
+            '--y',
+            `${y}px`
+        );
+
+    });
+
+});
+
 const glow = document.querySelector('.cursor-glow');
 
 if (glow) {
