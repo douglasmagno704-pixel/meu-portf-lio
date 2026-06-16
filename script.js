95d3860ed777e9c1eb9d56b62f5e1549e7f8003c
douglasmagno704-git <douglasmagno704@gmail.com>
2026-06-15 22:13:19 -0300

    pagina atualizada

--- script.js
+++ script.js
@@ -152,6 +152,45 @@ document.querySelectorAll(
 
 });
 
+const timeline = document.querySelector('.timeline');
+
+const observerTimeline = new IntersectionObserver((entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            timeline.classList.add('active');
+        }
+    });
+},{threshold:0.3});
+
+observerTimeline.observe(timeline);
+
+document.querySelectorAll('.projeto-card').forEach(card=>{
+
+    card.addEventListener('mousemove',(e)=>{
+
+        const rect = card.getBoundingClientRect();
+
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        const rotateY = (x / rect.width - 0.5) * 10;
+        const rotateX = (0.5 - y / rect.height) * 10;
+
+        card.style.transform =
+        `perspective(1000px)
+         rotateX(${rotateX}deg)
+         rotateY(${rotateY}deg)`;
+
+    });
+
+    card.addEventListener('mouseleave',()=>{
+
+        card.style.transform =
+        'perspective(1000px) rotateX(0) rotateY(0)';
+
+    });
+
+});
 
 
 // ==========================
